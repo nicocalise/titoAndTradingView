20716ac86a20acc8d5b8aa1b194ff24d6e4ec1b9
nicolas.calise <nicolas.calise@solera.com>
2024-07-30 13:17:52 +0200

    changes

--- api/index.js
+++ api/index.js
@@ -1,15 +1,6 @@
-const { handleWebhook } = require('../Services/listener');
+// api/index.js
+const webhooks = require('./webhooks');
 
-module.exports = async (req, res) => {
-    if (req.method === 'POST') {
-        try {
-            await handleWebhook(req, res);
-        } catch (error) {
-            console.error('Error processing webhook:', error);
-            res.status(500).json({ status: 'error', message: error.message });
-        }
-    } else {
-        res.setHeader('Allow', ['POST']);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
-    }
+module.exports = (req, res) => {
+  return webhooks(req, res);
 };
